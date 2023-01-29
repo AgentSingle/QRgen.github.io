0f345228aa2c35db10c63e988d71a7fc8f4e9d67
AtaOta <ataota.org@gmail.com>
2023-01-29 09:57:02 +0530

    commit VERSION-1.0.0

---
+++ js/qr.js
@@ -0,0 +1,60 @@
+let QR_Download_BTN = document.querySelector('#DOWNLOAD_QRcode');
+let qrContentInput = document.getElementById("qr-content");
+let QR_CONTAINER = document.querySelector('#qr-code');
+let dimension = QR_CONTAINER.getBoundingClientRect().height;
+let newDimension = (dimension - 10)
+QR_CONTAINER.style.width = `${dimension}px`;
+let qrCode;
+
+
+function generateQrCode(qrContent) {
+    const qr_code = new QRCode("qr-code", {
+            text: qrContent,
+            width: newDimension,
+            height: newDimension,
+            colorDark: "#009965",
+            colorLight: "#a3ffe0",
+            correctLevel: QRCode.CorrectLevel.H,
+        });
+    return qr_code;
+}
+
+
+const GenerateQR = (qrContent) => {
+    // let qrContentInput = document.getElementById("qr-content");
+    if (qrCode == null) {
+        // Generate code initially
+        qrCode = generateQrCode(qrContent);
+    } 
+    else {
+        // If code already generated then make
+        // again using same object
+        qrCode.makeCode(qrContent);
+    }
+}
+
+document.addEventListener("DOMContentLoaded", () => {
+    GenerateQR(qrContentInput.value);
+});
+qrContentInput.addEventListener('input', ()=>{
+    GenerateQR(qrContentInput.value);
+})
+
+// ADD DOWNLOAD BUTTON
+QR_Download_BTN.addEventListener('click', (e) => {
+    e.preventDefault();
+    if (QR_CONTAINER.lastElementChild){
+        let dataURL = QR_CONTAINER.lastElementChild.src;
+        let newElement = document.createElement('a');
+        newElement.setAttribute('href', dataURL);
+        newElement.setAttribute('download', 'qr_code.png');
+        newElement.style.display="none";
+        QR_Download_BTN.appendChild(newElement);
+        //onClick property
+        newElement.click();
+        QR_Download_BTN.removeChild(newElement);
+    }
+ 
+    
+})
+
