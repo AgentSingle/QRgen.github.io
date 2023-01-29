ad81c8daa32368637687925e35989b2f5ff06548
AtaOta <ataota.org@gmail.com>
2023-01-30 00:33:41 +0530

    commit VERSION-1.1.2

--- js/qr.js
+++ js/qr.js
@@ -1,12 +1,17 @@
 let QR_Download_BTN = document.querySelector('#DOWNLOAD_QRcode');
 let qrContentInput = document.getElementById("qr-content");
 let QR_CONTAINER = document.querySelector('#qr-code');
-let dimension = QR_CONTAINER.getBoundingClientRect().height;
+let QR_Display_CONTAINER = document.querySelector('#qrcode');
+let dimension = QR_Display_CONTAINER.getBoundingClientRect().height;
 let newDimension = (dimension - 10)
-QR_CONTAINER.style.width = `${dimension}px`;
+let LOGO = `${window.location.origin}/dummy/QR_icon_32x32.png`;
+QR_CONTAINER.style.height = `${dimension}`;
+QR_CONTAINER.style.width = `${dimension}`;
+QR_Display_CONTAINER.style.width = `${dimension}px`;
+QR_Display_CONTAINER.style.height = `${dimension}px`;
 let qrCode;
 
-
+// DRAW QR CODE
 function generateQrCode(qrContent) {
     const qr_code = new QRCode("qr-code", {
             text: qrContent,
@@ -19,35 +24,71 @@ function generateQrCode(qrContent) {
     return qr_code;
 }
 
-
+// INITIALIZE QR CODE
 const GenerateQR = (qrContent) => {
     // let qrContentInput = document.getElementById("qr-content");
     if (qrCode == null) {
         // Generate code initially
         qrCode = generateQrCode(qrContent);
+        AddLogoInQR(LOGO);
     } 
     else {
         // If code already generated then make
         // again using same object
         qrCode.makeCode(qrContent);
+        AddLogoInQR(LOGO);
     }
 }
 
+// AFTER LOADING WINDOW CALL INITIALIZATION FUNCTION
 document.addEventListener("DOMContentLoaded", () => {
     GenerateQR(qrContentInput.value);
 });
+
+// CHANGE QR CODE ACCORDING TO USER INPUT
 qrContentInput.addEventListener('input', ()=>{
     GenerateQR(qrContentInput.value);
 })
+
+// DRAW LOGON INSIDE CANVAS
+const AddLogoInQR = (LOGO) =>{
+    if (QR_CONTAINER.firstElementChild){
+        let NewCanvas = QR_CONTAINER.firstElementChild;
+        var img = new Image();
+        img.src = LOGO;
+        img.onload = function() {
+            var canvas = document.createElement("canvas");
+            canvas.width = img.width;
+            canvas.height = img.height;
+            var ctx = canvas.getContext("2d");
+            ctx.drawImage(img, 0, 0);
+
+            var img2 = new Image();
+            img2.src = NewCanvas.toDataURL();
+            img2.onload = function() {
+                var canvas2 = document.createElement("canvas");
+                canvas2.width = img2.width;
+                canvas2.height = img2.height;
+                var ctx2 = canvas2.getContext("2d");
+                ctx2.drawImage(img2, 0, 0);
+                ctx2.drawImage(img, (canvas2.width-img.width)/2, (canvas2.height-img.height)/2);
+                QR_Display_CONTAINER.innerHTML = "";
+                QR_Display_CONTAINER.appendChild(canvas2);
+            }
+        }
+    }
+}
+
 
 // ADD DOWNLOAD BUTTON
 QR_Download_BTN.addEventListener('click', (e) => {
     e.preventDefault();
     // console.warn(QR_CONTAINER.firstElementChild)
 
-    if (QR_CONTAINER.firstElementChild){
-        let canvas = QR_CONTAINER.firstElementChild;
-        var fileURL = canvas.toDataURL("image/png").replace("image/png", "image/octet-stream");
+    if (QR_Display_CONTAINER.firstElementChild){
+        let NewCanvas = QR_Display_CONTAINER.firstElementChild;
+
+        var fileURL = NewCanvas.toDataURL("image/png").replace("image/png", "image/octet-stream");
         // var fileURL = QR_CONTAINER.lastElementChild.src;
         fetch(fileURL)
         .then(response => response.blob())
@@ -70,6 +111,4 @@ QR_Download_BTN.addEventListener('click', (e) => {
             ), 3000);
         });
     }
- 
-    
 })
